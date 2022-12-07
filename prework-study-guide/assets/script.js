5960d7a966e4597584f9068388740825949f6b2e
William <wabarringer@gmail.com>
2022-12-07 12:35:05 -0800

    Added js to project

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -4,7 +4,7 @@ for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
    }
 
-/*
+
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -16,5 +16,5 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
+
 // text
